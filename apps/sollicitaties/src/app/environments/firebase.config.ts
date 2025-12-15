279014fae5e08cb7967d7c4ba65f2b9a470fd994
Marc <webdev@marcbakker.com>
2025-12-15 18:42:37 +0100

    add firebase

---
+++ apps/sollicitaties/src/app/environments/firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDbejCHlbBvslx_thj9gbfWsfz3pYXwZE4",
+  authDomain: "sollicitaties-marcb.firebaseapp.com",
+  projectId: "sollicitaties-marcb",
+  storageBucket: "sollicitaties-marcb.firebasestorage.app",
+  messagingSenderId: "18104146140",
+  appId: "1:18104146140:web:f3976c7176078028213c19",
+  measurementId: "G-KHD5H0W7J0"
+};

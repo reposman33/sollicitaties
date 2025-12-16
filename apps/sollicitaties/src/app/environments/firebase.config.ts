06754acf4e3201bfc6276eca815b13a401419f86
Marc <webdev@marcbakker.com>
2025-12-16 12:09:08 +0100

    Remove firebase.config.ts from git tracking

--- apps/sollicitaties/src/app/environments/firebase.config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAa81-6IgbFklLdL-nA74ZbFEZiiERRE00",
-  authDomain: "sollicitaties-marcb.firebaseapp.com",
-  projectId: "sollicitaties-marcb",
-  storageBucket: "sollicitaties-marcb.firebasestorage.app",
-  messagingSenderId: "18104146140",
-  appId: "1:18104146140:web:f3976c7176078028213c19",
-  measurementId: "G-KHD5H0W7J0"
-};
